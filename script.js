9992349f9a076fab1b946efa30fe7f1c591c8d1b
gklim516 <guankeng.lim@gmail.com>
2019-08-05 23:17:46 +0800

    Complete javascript to get and display products dynamically with JSON

--- script.js
+++ script.js
@@ -18,7 +18,25 @@ class Products {
     async getProducts() {
         try {
             let result = await fetch('products.json');
-            return result;
+            let data = await result.json();
+            let products = data.items;
+            products = products.map(item => {
+                const {
+                    title,
+                    price
+                } = item.fields;
+                const {
+                    id
+                } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {
+                    title,
+                    price,
+                    id,
+                    image
+                };
+            });
+            return products;
         } catch (error) {
             console.log(error);
         }
@@ -27,7 +45,27 @@ class Products {
 
 // display products
 class UI {
-
+    displayProducts(products) {
+        let result = '';
+        products.forEach(product => {
+            result += `
+            <!-- single product -->
+            <article class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product" class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        add to bag
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>$${product.price}</h4>
+            </article>
+            <!-- end of single product -->
+            `;
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 
 //local storage
@@ -40,5 +78,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const products = new Products();
 
     //get all products
-    products.getProducts().then(data => console.log(data));
-})
+    products.getProducts().then(products => ui.displayProducts(products));
+});
